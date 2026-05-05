5ad85aa49e1046c3a8d4704f1e4ced38774f50e5
Jason Park <jason.j.park79@gmail.com>
2026-05-05 13:23:34 -0400

    update title

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -10,7 +10,9 @@ export const PageHeader: FunctionComponent<PageHeaderProps> = ({
 }) => {
   return (
     <div className="px-4 pt-4 pb-3 border-b border-gray-100">
-      <h1 className="text-2xl font-semibold tracking-tight mb-1">Pokédex</h1>
+      <h1 className="text-2xl font-semibold tracking-tight mb-1">
+        Pokédex Virtualized
+      </h1>
       <p className="text-xs text-gray-400">{totalPokemonCount ?? 0} Pokémon</p>
     </div>
   )
