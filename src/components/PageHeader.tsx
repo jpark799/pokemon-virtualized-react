a1adc25ec5ab2b37b77f4635a08486b1dfc60e97
Jason Park <jason.j.park79@gmail.com>
2026-05-12 11:18:22 -0400

    add credit and link

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -13,7 +13,22 @@ export const PageHeader: FunctionComponent<PageHeaderProps> = ({
       <h1 className="text-2xl font-semibold tracking-tight mb-1">
         Pokédex Virtualized
       </h1>
-      <p className="text-xs text-gray-400">{totalPokemonCount ?? 0} Pokémon</p>
+      <div className="flex justify-between">
+        <p className="text-xs text-gray-400">
+          {totalPokemonCount ?? 0} Pokémon
+        </p>
+        <p className="text-xs text-gray-400">
+          made with ❤️ by
+          <a
+            href="https://www.linkedin.com/in/jpark799/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-1 text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-300 underline"
+          >
+            Jason Park
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
